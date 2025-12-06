f188cc6e13932790250f0994e113c59174db9c9c
mehdixbachir-cmyk <mehdixbachir@gmail.com>
2025-12-06 22:25:10 +0100

    Ajout message bonjour madame dans Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Salam Mehdi, hadi modification jdida!");
-
+        System.out.println("bonjour madame");
     }
 }
